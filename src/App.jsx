5546c8fe43799805c240b97a73b1be846fbf67cc
taha ahmed <tahahodhod650@gmail.com>
2026-09-10 02:57:23 +0300

    add hero and background photo

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ const App = () => {
       <div className="pattern">
         <div className="wrapper">
           <header>
+            <img src="./hero.png" alt="" />
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
           </header>
+
+          <p>Search</p>
         </div>
       </div>
     </main>
