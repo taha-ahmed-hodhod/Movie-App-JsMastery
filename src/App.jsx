fe221b2ad27908f15ab5ff79e3edf7152f259cea
taha ahmed <tahahodhod650@gmail.com>
2026-09-10 02:37:23 +0300

    initial tailwindCss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
